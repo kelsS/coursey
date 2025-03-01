9a3fdd8db92b6388667b83d93598a7db255f0ff4
kelsS <ratherkelsey@gmail.com>
2025-03-01 17:24:01 -0500

    started setting app pages and added course data

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,7 @@
+<template>
+    <h2>Lesson</h2>
+
+    <p>This is a lesson</p>
+    <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
+    <p>Lesson slug: {{ $route.params.lessonSlug }}</p>
+</template>
