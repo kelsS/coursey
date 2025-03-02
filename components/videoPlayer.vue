55d920fa8dd070080f4ad620549cd9874edbd924
kelsS <ratherkelsey@gmail.com>
2025-03-01 21:41:26 -0500

    updated course page

---
+++ components/videoPlayer.vue
@@ -0,0 +1,24 @@
+<!-- player src is dynamic -->
+<template>
+  <iframe
+    width="560"
+    height="315"
+    :src="`https://player.vimeo.com/video/${props.videoId}`"
+    title="Video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>
+</template>
+
+<script lang="ts" setup>
+// define the video idea prop
+const props = defineProps({
+    videoId: {
+        type: Number,
+        requred: true,
+    },
+});
+</script>
+
+<style scoped></style>
--- pages/course.vue
+++ pages/course.vue
@@ -1,28 +1,48 @@
 <template>
     <div
       class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center">
-      <div class="prose mb-12">
+      <header class="prose mb-12">
         <h1>
           <span class="font-medium">
             Course:
             <span class="font-bold">Mastering Nuxt 3</span>
           </span>
         </h1>
-      </div>
+      </header>
   
       <div class="flex flex-row justify-center flex-grow">
-        <div
-          class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col">
+        <nav
+          class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col" aria-label="Chapters">
           <h2>Chapters</h2>
           <!-- All the lessons for the course listed here -->
-        </div>
+          <!-- render course -->
+          <div
+            class="space-y-1 mb-4 flex flex-col"
+            v-for="chapter in chapters"
+            :key="chapter.slug">
+            <h3>{{ chapter.title }}</h3>
+                <NuxtLink
+                    v-for="(lesson, index) in chapter.lessons"
+                    :key="lesson.slug"
+                    class="flex flex-row space-x-1 no-underline prose-sm font-normal py-1 px-4 -mx-4"
+                    :to="`/course/chapter/${chapter.slug}/lesson/${lesson.slug}`">
+                    <span class="text-gray-500">{{ index + 1 }}.</span>
+                    <span>{{ lesson.title }}</span>
+                </NuxtLink>
+            </div>
+        </nav>
   
         <div class="prose p-12 bg-white rounded-md w-[65ch]">
           <h2>Lesson</h2>
           <p>This is a lesson</p>
-          
+
           <NuxtPage />
         </div>
       </div>
     </div>
 </template>
+
+<script setup>
+// get chapters
+const { chapters } = useCourse();
+</script>
