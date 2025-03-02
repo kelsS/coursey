8cbbc4a6a39dae037d94dccfdb148e1ee1864fd2
kelsS <ratherkelsey@gmail.com>
2025-03-01 20:36:36 -0500

    updated lesson slug vue

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,7 +1,30 @@
 <template>
     <h2>Lesson</h2>
-
     <p>This is a lesson</p>
-    <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
-    <p>Lesson slug: {{ $route.params.lessonSlug }}</p>
-</template>
+    <p>{{ chapter.title }}</p>
+    <p>{{ lesson.title }}</p>
+  </template>
+  
+  <script setup>
+  // get course
+  const course = useCourse();
+  // get route
+  const route = useRoute();
+  
+  // get chapter by chapter slug
+  const chapter = computed(() => {
+    return course.chapters.find(
+      (chapter) => chapter.slug === route.params.chapterSlug
+    );
+  });
+  
+  // get lesson by lesson slug
+  const lesson = computed(() => {
+    return chapter.value.lessons.find(
+      (lesson) => lesson.slug === route.params.lessonSlug
+    );
+  });
+
+  // show course data in console
+  console.log(course);
+  </script>
