2f71de836b2200eca78ef7a97be955020157cee5
kelsS <ratherkelsey@gmail.com>
2025-03-11 15:17:17 -0400

    made iframe video player responsive

--- components/videoPlayer.vue
+++ components/videoPlayer.vue
@@ -1,14 +1,17 @@
 <!-- player src is dynamic -->
 <template>
-  <iframe
-    width="560"
-    height="315"
+  <div class="video-player-frame">
+    <iframe
+    width=""
+    height=""
     :src="`https://player.vimeo.com/video/${props.videoId}`"
     title="Video player"
     frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     allowfullscreen
+    class="iframe-responsive"
   ></iframe>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -21,4 +24,23 @@ const props = defineProps({
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+
+  .video-player-frame {
+    position: relative;
+    overflow: hidden;
+    width: 100%;
+    padding-top: 56.25%; /* 16:9 Aspect Ratio (divide 9 by 16 = 0.5625) */
+  }
+
+  .iframe-responsive {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+</style>
